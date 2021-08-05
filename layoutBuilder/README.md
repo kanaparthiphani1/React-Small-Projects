5ff5fb8fa82bcf9af9dc5be2e7064cba46370c50
kanaparthiphani1 <38982795+kanaparthiphani1@users.noreply.github.com>
2021-08-06 00:17:53 +0530

    [Project-8] - Small Layout Builder
    Done by using React Context

---
+++ layoutBuilder/README.md
@@ -0,0 +1,9 @@
+In this Project I built a **Layout Builder App** by applying the concepts of React Context
+
+### Refer to the image below:
+
+<br/>
+<div style="text-align: center;">
+    <img src="https://assets.ccbp.in/frontend/content/react-js/layout-builder-output.gif" alt="Layout Builder Output" style="max-width:70%;box-shadow:0 2.8px 2.2px rgba(0, 0, 0, 0.12)">
+</div>
+<br/>
---
+++ layoutBuilder/src/components/ConfigurationController/index.js
@@ -0,0 +1,72 @@
+// Write your code here
+import ConfigurationContext from '../../context/ConfigurationContext'
+
+import './index.css'
+
+const ConfigurationController = () => (
+  <ConfigurationContext.Consumer>
+    {value => {
+      const {
+        showContent,
+        showLeftNavbar,
+        showRightNavbar,
+        onToggleShowContent,
+        onToggleShowLeftNavbar,
+        onToggleShowRightNavbar,
+      } = value
+      const onChangeContent = () => {
+        onToggleShowContent()
+      }
+      const onChangeLeftNavbar = event => {
+        onToggleShowLeftNavbar(event.target.value)
+      }
+      const onChangeRightNavbar = event => {
+        onToggleShowRightNavbar(event.target.value)
+      }
+      return (
+        <div className="configuration-controls-container">
+          <div className="responsive-controls-container">
+            <h1 className="layout-heading">Layout</h1>
+            <div className="checkbox-group">
+              <div className="checkbox-container">
+                <input
+                  type="checkbox"
+                  id="content"
+                  checked={showContent}
+                  onChange={onChangeContent}
+                />
+                <label className="label-text" htmlFor="content">
+                  Content
+                </label>
+              </div>
+              <div className="checkbox-container">
+                <input
+                  type="checkbox"
+                  id="leftNavbar"
+                  checked={showLeftNavbar}
+                  onChange={onChangeLeftNavbar}
+                />
+                <label className="label-text" htmlFor="leftNavbar">
+                  Left Navbar
+                </label>
+              </div>
+              <div className="checkbox-container">
+                <input
+                  type="checkbox"
+                  id="rightNavbar"
+                  checked={showRightNavbar}
+                  onChange={onChangeRightNavbar}
+                />
+                <label className="label-text" htmlFor="rightNavbar">
+                  Right Navbar
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }}
+  </ConfigurationContext.Consumer>
+)
+
+export default ConfigurationController
